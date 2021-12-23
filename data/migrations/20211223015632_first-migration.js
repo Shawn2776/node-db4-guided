4d2f67bffdd2046c685e09771671dada6dc35a72
Dave Kidd <dave@davekidd.net>
2021-12-22 19:09:57 -0700

    wip

--- data/migrations/20211223015632_first-migration.js
+++ data/migrations/20211223015632_first-migration.js
@@ -26,7 +26,7 @@ exports.up = function(knex) {
 exports.down = function(knex) {
     return knex.schema
     .dropTableIfExists()
-    .dropTableIfExists()
+    .dropTableIfExists("animals")
     .dropTableIfExists("species")
     .dropTableIfExists("zoos")
 };
